d264ff56a754949af4f93f3f5c34a6d8d441b9df
BrayanVarga <167211070+BrayanVarga@users.noreply.github.com>
2024-04-21 21:10:23 -0500

    Add files via upload

---
+++ CSemana03/CSemana03/src/csemana03/Operaciones_1.java
@@ -0,0 +1,12 @@
+package csemana03;
+
+import java.util.Arrays;
+
+public class Operaciones_1 {
+
+    public double[] ordenarDatos(double[] datos) {
+        Arrays.sort(datos);
+        return datos;
+    }
+}
+
